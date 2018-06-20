5e28377ac9eb4130f363ec538d528b469cc9c7a9
Desenvolvimento 2 <>
2018-06-20 12:37:18 -0300

    Alteração do firebase js

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,10 +1,10 @@
 export default {
-  apiKey: "AIzaSyDxUNYI9_eNkEAAZRJmz8QjDEsG2Fu-zxY",
-  authDomain: "imobi-cbf7c.firebaseapp.com",
-  databaseURL: "https://imobi-cbf7c.firebaseio.com",
-  projectId: "imobi-cbf7c",
-  storageBucket: "imobi-cbf7c.appspot.com",
-  messagingSenderId: "622442594569"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
     
@@ -13,9 +13,3 @@ export default {
 
 
 
-  // apiKey: "AIzaSyDxUNYI9_eNkEAAZRJmz8QjDEsG2Fu-zxY",
-  // authDomain: "imobi-cbf7c.firebaseapp.com",
-  // databaseURL: "https://imobi-cbf7c.firebaseio.com",
-  // projectId: "imobi-cbf7c",
-  // storageBucket: "imobi-cbf7c.appspot.com",
-  // messagingSenderId: "622442594569""
